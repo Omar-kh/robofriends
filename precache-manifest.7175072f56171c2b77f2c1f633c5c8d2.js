394685466bf65f9328e63331f13165b122bb48fe
Omar_kh <omar.kadiri@hotmail.com>
2019-02-08 21:06:28 +0100

    Updates

---
+++ precache-manifest.7175072f56171c2b77f2c1f633c5c8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Omar-kh/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3b0dab2661a8a8d60e65",
+    "url": "/Omar-kh/robofriends/static/js/runtime~main.3b0dab26.js"
+  },
+  {
+    "revision": "6b7b1659e357ab228b53",
+    "url": "/Omar-kh/robofriends/static/js/main.6b7b1659.chunk.js"
+  },
+  {
+    "revision": "772eb3c264415604924b",
+    "url": "/Omar-kh/robofriends/static/js/1.772eb3c2.chunk.js"
+  },
+  {
+    "revision": "6b7b1659e357ab228b53",
+    "url": "/Omar-kh/robofriends/static/css/main.4cccc50a.chunk.css"
+  },
+  {
+    "revision": "772eb3c264415604924b",
+    "url": "/Omar-kh/robofriends/static/css/1.0a9b5a2f.chunk.css"
+  },
+  {
+    "revision": "41fe315e3807975fa6985b457cba7abb",
+    "url": "/Omar-kh/robofriends/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Omar-kh/robofriends/precache-manifest.7175072f56171c2b77f2c1f633c5c8d2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Omar-kh/robofriends/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
