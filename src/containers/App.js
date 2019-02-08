3aa27fa87394e882731fbdd1e2cc933c7ddca1fb
Omar_kh <omar.kadiri@hotmail.com>
2019-02-08 20:59:32 +0100

    Finish last touches before deploy and turn into PWA

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
+import Header from '../components/Header';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
@@ -40,7 +41,7 @@ class App extends Component {
         <h1>Loading</h1> :
         (
                 <div className="tc">
-                    <h1 className="f1">Robofriends</h1>
+                    <Header/>
                     <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
