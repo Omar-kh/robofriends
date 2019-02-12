6e571a41d23ea953c8e2b1cec349de14b58b4585
Omar_kh <omar.kadiri.hassani@gmail.com>
2019-02-12 17:54:45 +0100

    Prettiy files

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
-const rootReducer = combineReducers({searchRobots, requestRobots});
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-        <Provider store={store} >
-                < App/>
-        </Provider>
-    
-, document.getElementById('root'));
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
